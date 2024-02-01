1b8e2290ca8217d6b1e52577beb522585e1f0b9e
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-02-01 16:50:49 +0100

    project start

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Skill;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        $technologies = Technology::all();
+        $skills = Skill::all();
+        $user = Auth::user();
+        return view('admin.projects.index', compact('projects', 'technologies', 'skills', 'user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+        $technologies = Technology::all();
+        return view('admin.projects.create' , compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  StoreProjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        // dd($request);
+        $form_data = $request->validated();
+        // dd($form_data);
+        $project = new Project;
+
+        $project->fill($form_data);
+        // dd($project);
+        $project->save();
+
+        return redirect()->route('admin.projects.show', ['project' => $project->slug]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project, Technology $technology, Skill $skill)
+    {
+        $user = Auth::user();
+        return view('admin.projects.show', compact('project', 'technology', 'skill', 'user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        return view('admin.projects.edit', compact('project'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateProjectRequest  $request
+     * @param  Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $form_data = $request->validated();
+        $project->update($form_data);
+
+        return redirect()->route('admin.projects.show', ['project' => $project->slug]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect()->route('admin.projects.index')->with('message', '$project->title e\' stato eliminato.');
+    }
+}
